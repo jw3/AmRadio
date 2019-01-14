3bf6ffc3d80a02225116ef92ed7985965c34dac8
John Wass <jwass3@gmail.com>
2019-01-13 22:43:07 -0500

    cleanup

--- app/src/main/java/com/github/jw3/amradio/LegacyStreamFragment.kt
+++ app/src/main/java/com/github/jw3/amradio/LegacyStreamFragment.kt
@@ -21,27 +21,15 @@ import okhttp3.OkHttpClient
 import java.util.logging.Logger
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [LegacyStreamFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [LegacyStreamFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class LegacyStreamFragment : Fragment() {
     var p1: SimpleExoPlayer? = null
     var p2: SimpleExoPlayer? = null
     var p3: SimpleExoPlayer? = null
 
     private var listener: OnFragmentInteractionListener? = null
-
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -73,9 +61,7 @@ class LegacyStreamFragment : Fragment() {
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_legacy_stream, container, false)
     }
 
